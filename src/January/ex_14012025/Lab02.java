c31d81c2c67ab7a76a759720f5b2a1f744de61cb
Saurabh <saurabhb079@gmail.com>
2025-01-14 13:01:05 +0530

    I have done some changes.

--- src/January/ex_14012025/Lab02.java
+++ src/January/ex_14012025/Lab02.java
@@ -4,6 +4,6 @@ package January.ex_14012025;
 public class Lab02 {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
